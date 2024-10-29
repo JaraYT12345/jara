6c734c932c85bdf33e9432f603a709fb04d9ff0c
JaraYT12345 <fernando.jara.delgadillo@alumnos.sip.cl>
2024-10-29 08:59:22 -0300

    first commit

---
+++ java.js
@@ -0,0 +1,9 @@
+// Función para mostrar una alerta
+function mostrarAlerta(demostracion) {
+    alert('Has hecho clic en ' + demostracion);
+}
+
+// Función para cambiar el color de fondo del cuadro
+function cambiarColor(elemento, color) {
+    elemento.style.backgroundColor = color;
+}
